76a5e021e8863479a4ec09b2ded3facf295899e8
Jahnavi18104 <jahnavimeesala333@gmail.com>
2024-11-06 20:28:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,8 +16,9 @@ app.use(express.json());
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
-    .catch(err => { console.error('MongoDB connection error:', err);
-                   process.exit(1)});
+    .catch(err => { console.error('MongoDB connection error:', err)
+                   process.exit(1);
+                  });
 
 // Use routes
 app.use('/api/donors', donorRoutes); // API route for donors
